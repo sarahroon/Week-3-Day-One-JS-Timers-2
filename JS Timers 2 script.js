65421134a929848acce2a2bf3b53c1724ffc7a42
sarahroon <sarahrooney130500@gmail.com>
2026-01-19 14:04:11 +0000

    Update JS Timers 2 script.js

--- JS Timers 2 script.js
+++ JS Timers 2 script.js
@@ -1,17 +1,11 @@
+let timerId;
 let seconds = 0;
-let timer;
 
-timer = setInterval(() => {
-  seconds++;
-  document.getElementById("timer").textContent = seconds;
+document.getElementById("startBtn").addEventListener("click", () => {
+    if (timerId) return;
 
-  if (seconds >= 5) {
-    document.getElementById("stopBtn").disabled = false;
-  }
-}, 1000);
-
-document.getElementById("stopBtn").addEventListener("click", () => {
-  clearInterval(timer);
-  document.getElementById("timer").textContent = "Stopped at " + seconds + " seconds";
+    timerId = setInterval(() => {
+       seconds++;
+      document.getElementById("output").textContent = seconds;
+    }, 1000);
 });
-

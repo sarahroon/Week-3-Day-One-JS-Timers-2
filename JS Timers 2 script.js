17973b7e008e7e03af6cee7db1b090c0edd7cec7
sarahroon <sarahrooney130500@gmail.com>
2026-01-19 13:33:17 +0000

    Update JS Timers 2 script.js

--- JS Timers 2 script.js
+++ JS Timers 2 script.js
@@ -1,1 +1,13 @@
+// TIMER - using setInterval for a regular-interval timer that clears after 5 seconds
 
+function say(Hello) {
+  console.log("Hello world!");
+}
+
+const myInterval = setInterval(() => {
+  console.log("I`m a message!");
+},  1000);
+
+setTimeout(() => {
+  clearInterval(myInterval);
+}, 5000);
